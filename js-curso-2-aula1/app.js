2554e9d296172f17b71b7c7dce387078c7e4a861
Iann Gabriel <iann.domingos@gmail.com>
2025-07-30 08:45:20 -0300

    atualizando exercicios

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Digite um número de 01 a 10: ';
+
+
+function verificarChute(){
+    console.log('Acessamos a função');
+}
