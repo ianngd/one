59cee7ded9e11d9cdcaed9b55064387fe2c4ba53
Iann Gabriel <iann.domingos@gmail.com>
2025-08-05 21:46:15 -0300

    avançando nos modulos da ONE

--- exercicios-logica2/app.js
+++ exercicios-logica2/app.js
@@ -1,28 +1,57 @@
-// Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
-let titulo = document.querySelector('h1');
-titulo.innerHTML= 'Hora do Desafio';
+//Criar uma função que exibe "Olá, mundo!" no console.
+console.log('Olá mundo!');
 
-// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
-function verificarConsole(){
-    console.log ('O botão foi clicado');
+//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function hello(nome) {
+    console.log(`Olá, ${nome}!`);
 }
-// Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
-function verificarAlert() {
-    alert('Eu amo JS');
-}
-// Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
-function verificarPrompt(){
-    let cidade = prompt('Digite uma cidade do brasil');
-    alert(`Estive em ${cidade} e lembrei de você!`);
-}
-// Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
 
-function verificarSoma(){
-    let valor1 = 0
-    valor1 = prompt('Digite um valor');
-    let valor2 = 0
-    valor2 = prompt('Digite um segundo valor');
-    let soma = 0
-    soma = valor1 + valor2;
-    alert(` ${valor1} + ${valor2} = ${soma}`);
+nome = prompt('Qual seu nome?');
+hello(nome);
+
+
+//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function dobroNumero(num) {
+    return num*2; 
 }
+
+num = parseFloat(prompt('Digite um número:'));
+console.log(`O dobro deste número é, ${dobroNumero(num)}`);
+
+
+
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function media(num1, num2, num3){
+    return (num2 + num3 + num1) / 3;
+}
+
+num1 = parseFloat(prompt('Digite o primeiro número:'));
+num2 = parseFloat(prompt('Digite o segundo número:'));    
+num3 = parseFloat(prompt('Digite o terceiro número:'));
+
+console.log(`A média dos 3 números é: ${media(num1, num2, num3)}`);
+
+
+//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+function verificarMaior(num1, num2){
+    if (num1 > num2) {
+        return num1;
+    }
+    else {
+        return num2;    
+    }    
+}
+
+num1 = parseFloat(prompt('Digite o primeiro número:'));
+num2 = parseFloat(prompt('Digite o segundo número:'));
+console.log(`O maior número é: ${verificarMaior(num1, num2)}`);
+
+
+//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+
+function quadradoNumero(num) {
+    return num * num;
+}   
+num = parseFloat(prompt('Digite um número:'));
+console.log(`O quadrado deste número é: ${quadradoNumero(num)}`);
---
+++ exercicios-logica2/m1.js
@@ -0,0 +1,25 @@
+// Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+let titulo = document.querySelector('h1');
+titulo.innerHTML= 'Hora do Desafio';
+
+// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+function verificarConsole(){
+    console.log ('O botão foi clicado');
+}
+// Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+function verificarAlert() {
+    alert('Eu amo JS');
+}
+// Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+function verificarPrompt(){
+    let cidade = prompt('Digite uma cidade do brasil');
+    alert(`Estive em ${cidade} e lembrei de você!`);
+}
+// Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
+
+function verificarSoma() {
+    let a = Number(prompt("Digite um número:"));
+    let b = Number(prompt("Digite outro número:"));
+    let soma = a + b;
+    alert(`Resultado: ${soma}`);
+}
